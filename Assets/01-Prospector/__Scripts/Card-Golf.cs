910daa96a79261bdbf8b40940b5d3f876a7ba6c6
Hamza <79418669+HA-Hamza-GITHUB@users.noreply.github.com>
2024-03-29 00:17:00 -0400

    Readjusting layout
    Layout changes other than column change would break the code

--- Assets/01-Prospector/__Scripts/Card-Golf.cs
+++ Assets/01-Prospector/__Scripts/Card-Golf.cs
@@ -2,27 +2,90 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public enum eCardStateGolf
-{
-    drawpile,
-    tableau,
-    target,
-    discard
-}
+public class CardGolf : MonoBehaviour {
 
-public class CardGolf : MonoBehaviour
-{
-    [Header("Set Dynamically: CardGolf")]
-    public eCardStateGolf state = eCardStateGolf.drawpile;
-    public List<CardGolf> hiddenBy = new List<CardGolf>();
-    public int layoutID;
-    public SlotDef slotDef;
+    public string suit;
+    public int rank;
+    public Color color = Color.black;
+    public string colS = "Black";  // or "Red"
+    
+    public List<GameObject> decoGOs = new List<GameObject>();
+    public List<GameObject> pipGOs = new List<GameObject>();
+    
+    public GameObject back;  // back of card;
+    public CardDefinition def;  // from DeckXML.xml		
+    public SpriteRenderer[] spriteRenderers;
 
-    // Additional properties specific to Golf Solitaire can be added here
-
-    // This method can be extended or overridden to include specific behavior for Golf Solitaire
-    public virtual void OnMouseUpAsButton()
+    void Start()
     {
-        // Additional logic for card interaction in Golf Solitaire
+        SetSortOrder(0); // Ensures that the card starts properly depth sorted
     }
-}
+
+    // If spriteRenderers is not yet defined, this function defines it
+    public void PopulateSpriteRenderers()
+    {
+        // If spriteRenderers is null or empty
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            // Get SpriteRenderer Components of this GameObject and its children
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    // Sets the sortingLayerName on all SpriteRenderer Components
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    // Sets the sortingOrder of all SpriteRenderer Components
+    public void SetSortOrder(int sOrd)
+    { // a
+        PopulateSpriteRenderers();
+        // Iterate through all the spriteRenderers as tSR
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                // If the gameObject is this.gameObject, it's the background
+                tSR.sortingOrder = sOrd; // Set it's order to sOrd
+                continue; // And continue to the next iteration of the loop
+            }
+            // Each of the children of this GameObject are named
+            // switch based on the names
+            switch (tSR.gameObject.name)
+            {
+                case "back":
+                    // if the name is "back"
+                    // Set it to the highest layer to cover the other sprites
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+                case "face": // if the name is "face"
+                default: // or if it's anything else
+                    // Set it to the middle layer to be above the background
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
+
+    public bool faceUp {
+        get {
+            return (!back.activeSelf);
+        }
+
+        set {
+            back.SetActive(!value);
+        }
+    }
+
+    // Virtual methods can be overridden by subclass methods with the same name
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name); // When clicked, this outputs the card name
+    }
+} // class Card
